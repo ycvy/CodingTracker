e06884ab881a6cf9e69e4c763d2341b6a2d6f2e6
Max <105699875+ycvy@users.noreply.github.com>
2024-04-05 10:18:09 +0200

    Keeping one message

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace CodingTracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
