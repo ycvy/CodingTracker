32c7712ecb71349fe64caf8cc849b4ff82fb8e37
Max <105699875+ycvy@users.noreply.github.com>
2024-04-05 10:13:31 +0200

    Update Console.WriteLine

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace CodingTracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
